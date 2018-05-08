86cc5251f1f9337852070a4d2600b8a3ea9513d7
mamadou aliou barry <mamadou1fetowel@yahoo.fr>
2018-05-08 17:59:53 +0200

    quelques modif

--- Wed/www/javascript/index.js
+++ Wed/www/javascript/index.js
@@ -90,3 +90,28 @@ function loadFile() {
   xhr.send(null); // La requête est prête, on envoie tout !
 
 }
+
+
+/*partie modal*/
+
+function lanceModal(elem) {
+  var modal = document.getElementById('myModal');
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementsByClassName('image')[elem];
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+          img.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+      }
+
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+          modal.style.display = "none";
+    }
+  }
